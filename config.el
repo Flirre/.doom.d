582dba1311b377e527bba509bb26c95f06bf5c72
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-09-10 08:11:36 +0200

    updated font settings for mac

--- config.el
+++ config.el
@@ -19,10 +19,15 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
+(when (eq system-type 'darwin)
+(setq doom-font (font-spec :family "FantasqueSansMono Nerd Font" :size 18 )
+       doom-variable-pitch-font (font-spec :family "sans" :size 13)
+       doom-big-font (font-spec :family "FantasqueSansMono Nerd Font" :size 22 )))
+
+(when (eq system-type 'gnu/linux)
  (setq doom-font (font-spec :family "FantasqueSansMono Nerd Font" :size 22 )
        doom-variable-pitch-font (font-spec :family "sans" :size 13)
-       doom-big-font (font-spec :family "FantasqueSansMono Nerd Font" :size 22 ))
-
+       doom-big-font (font-spec :family "FantasqueSansMono Nerd Font" :size 22 )))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
