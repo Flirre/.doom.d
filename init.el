b82d35958b8f03d0b9b96a79f558284157b1bfb3
Filip Lindahl <filip.g.lindahl@gmail.com>
2021-08-09 22:01:40 +0200

    update java setup

--- init.el
+++ init.el
@@ -134,8 +134,8 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
+       (java +lsp) ; the poster child for carpal tunnel syndrome
        (json +lsp)              ; At least it ain't XML
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
